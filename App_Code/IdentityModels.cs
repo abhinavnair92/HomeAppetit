74bb5bdffb4854d5999a9901834c808a070ea890
aamotor <aamotor@bu.edu>
2014-04-25 18:46:26 -0400

    Stale Files

--- App_Code/IdentityModels.cs
+++ App_Code/IdentityModels.cs
@@ -10,6 +10,7 @@ namespace _512_Project_Master
     // You can add User data for the user by adding more properties to your User class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
